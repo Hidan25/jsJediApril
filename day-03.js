1444a3cd9f786bcaacf47c710e529e6c1ea8ec5f
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-04-24 23:38:37 +0300

    Create day-03.js

---
+++ day-03.js
@@ -0,0 +1,15 @@
+tasl 01
+https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+function trueOrFalse(val) {
+  if (val == false) {
+    return "false";
+  } else if (val == undefined) {
+    return "false";
+  } else if (val == null) {
+    return "false";
+  } else {
+    return "True";
+  }
+}
+
+task 02 
