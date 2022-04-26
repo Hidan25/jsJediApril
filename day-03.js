8f4ef391333801ce68072db6f07e7c73103ceadc
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-04-26 21:00:08 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -1,4 +1,4 @@
-tasl 01
+task 01
 https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
 function trueOrFalse(val) {
   if (val == false) {
@@ -13,3 +13,15 @@ function trueOrFalse(val) {
 }
 
 task 02 
+https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
+function saleHotdogs(n) {
+  if (n < 5) {
+    return 100 * n;
+  } else if (n >= 5 && n < 10) {
+    return 95 * n;
+  } else if (n >= 10) {
+    return 90 * n;
+  }
+}
+
+task 03
