fcc63e097588560ca04c0bfc98a84ca3ca540908
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-04-27 15:28:36 +0300

    Update day-03.js

--- day-03.js
+++ day-03.js
@@ -27,25 +27,26 @@ function saleHotdogs(n) {
 task 03
 https://www.codewars.com/kata/572059afc2f4612825000d8a
 function howManydays(month) {
-  var days;
   switch (month) {
-    case 1:
-      return 31;
-      break;
     case 2:
       return 28;
       break;
+    case 1:
     case 3:
-      return 31;
-      break;
-    case 4:
-      return 30;
-      break;
+    case 5:
+    case 7:
+    case 8:
+    case 10:
     case 12:
       return 31;
       break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+      break;
     default:
-      return "Enter another month :)";
+      return " Enter the current month :)";
   }
-  return days;
 }
