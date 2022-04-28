619fd035cd81acb18f22964e3e0a3a469a958744
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-04-28 21:49:29 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -27,3 +27,18 @@ function pickIt(arr) {
   }
   return [odd, even];
 }
+task 03
+http://www.codewars.com/kata/5721c189cdd71194c1000b9b
+function grabDoll(dolls) {
+  var bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] == "Hello Kitty" || dolls[i] == "Barbie doll") {
+      bag.push(dolls[i]);
+      if (bag.length == 3) break;
+    } else {
+      continue;
+    }
+  }
+
+  return bag;
+}
