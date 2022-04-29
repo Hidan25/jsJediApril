f3ab2c928df6be52b5570107df7cc7060493fbf0
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-04-29 13:20:24 +0300

    Create day-05.js

---
+++ day-05.js
@@ -0,0 +1,9 @@
+Task 01
+https://www.codewars.com/kata/is-this-my-tail/train/javascript
+function correctTail(body, tail) {
+  sub = body.substr(body.length - tail.length);
+  if ((sub = tail));
+  return true;
+}
+Task 02
+https://www.codewars.com/kata/56f6ad906b88de513f000d96
