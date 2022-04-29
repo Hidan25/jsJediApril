3836431e00a38544005c0effedfcfae6d3b572fa
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-04-29 12:26:26 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -42,3 +42,21 @@ function grabDoll(dolls) {
 
   return bag;
 }
+Task 04
+http://www.codewars.com/kata/5722b3f0bd5583cf44001000
+function giveMeFive(obj) {
+  let five = [];
+  for (let key in obj) {
+    if (key.length == 5) five.push(key);
+    if (obj[key].length == 5) five.push(obj[key]);
+  }
+  return five;
+}
+function giveMeFive(obj) {
+  let five = [];
+  for (let key in obj) {
+    if (key.length == 5) five.push(key);
+    if (obj[key].length == 5) five.push(obj[key]);
+  }
+  return five;
+}
