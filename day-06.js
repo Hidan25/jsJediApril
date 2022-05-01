3885ae7f785e160d4091d6e7fe86ba56e5cd868f
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2022-05-01 03:25:06 +0300

    Create day-06.js

---
+++ day-06.js
@@ -0,0 +1,24 @@
+task 01
+https://www.codewars.com/kata/maximum-multiple/train/javascript
+function maxMultiple(divisor, bound) {
+  let num = 0;
+  while (num < bound) {
+    num = num + divisor;
+    if (num > bound) {
+      return num - divisor;
+    }
+  }
+  return num;
+}
+
+Task 02
+https://www.codewars.com/kata/is-he-gonna-survive/train/javascript
+function hero(bullets, dragons) {
+  return dragons * 2 <= bullets;
+}
+Task 03 
+https://www.codewars.com/kata/59441520102eaa25260000bf
+function unusualFive() {
+  let arr = ['zero','one','two','three','four'];
+  return arr.length;
+}
